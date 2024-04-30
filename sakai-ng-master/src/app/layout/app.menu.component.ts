99a602ddfa3890b6bb0c5fc51445210c90cb2463
Mine25-afk <122749969+Mine25-afk@users.noreply.github.com>
2024-04-30 15:24:59 -0600

    ggagag

---
+++ sakai-ng-master/src/app/layout/app.menu.component.ts
@@ -0,0 +1,88 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                items: [
+                    {
+                        label: 'Acceso',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Usuarios',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/uikit/departamento']
+                            },
+                            {
+                                label: 'Roles',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/auth/error']
+                            },
+                        ]
+                    },
+                    
+                 
+
+                ]
+            },
+            {
+                items: [
+                    {
+                        label: 'Generales',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Departamentos',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login']
+                            },
+                            {
+                                label: 'Municipios',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/auth/error']
+                            },
+                        ]
+                    },
+                    
+                 
+
+                ]
+            },
+            {
+                items: [
+                    {
+                        label: 'Ventas',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Joyas',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login']
+                            },
+                            {
+                                label: 'Maquillajes',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/auth/error']
+                            },
+                        ]
+                    },
+                    
+                 
+
+                ]
+            },
+        ];
+    }
+}
