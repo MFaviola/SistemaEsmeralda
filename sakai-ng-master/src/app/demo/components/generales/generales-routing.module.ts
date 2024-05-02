58f79f4a847abaa5b75c4f167294bd13eaedbef7
Mine25-afk <122749969+Mine25-afk@users.noreply.github.com>
2024-05-02 00:49:46 -0600

    agagagag

--- sakai-ng-master/src/app/demo/components/generales/generales-routing.module.ts
+++ sakai-ng-master/src/app/demo/components/generales/generales-routing.module.ts
@@ -8,7 +8,11 @@ import { RouterModule } from '@angular/router';
 
        
         { path: 'categoria', data: { breadcrumb: 'Categoria' }, loadChildren: () => import('./Categoria/list-categoria/Categoriademo.module').then(m => m.CategoriaDemoModule) },
-
+        { path: 'cargo', data: { breadcrumb: 'Cargo' }, loadChildren: () => import('./Cargo/list-cargo/Cargodemo.module').then(m => m.CargoDemoModule) },
+        { path: 'material', data: { breadcrumb: 'Material' }, loadChildren: () => import('./Materiales/list-material/Materialdemo.module').then(m => m.MaterialDemoModule) },
+        { path: 'metodo', data: { breadcrumb: 'Metodo' }, loadChildren: () => import('./MetodoPago/list-metodopago/Metododemo.module').then(m => m.MetododemoDemoModule) },
+        { path: 'proveedor', data: { breadcrumb: 'Proveedor' }, loadChildren: () => import('./Proveedor/list-proveedor/Proveedordemo.module').then(m => m.ProveedorDemoModule) },
+        { path: 'sucursal', data: { breadcrumb: 'Sucursal' }, loadChildren: () => import('./Sucursales/list-sucursal/Sucursaldemo.module').then(m => m.SucursalDemoModule) },
         { path: 'estado', data: { breadcrumb: 'Estado' }, loadChildren: () => import('./EstadoCivil/list-estadocivil/EstadoCivildemo.module').then(m => m.EstadoCivilDemoModule) },
         { path: 'impuesto', data: { breadcrumb: 'Impuesto' }, loadChildren: () => import('./Impuesto/list-impuesto/Impuestodemo.module').then(m => m.ImpuestoDemoModule) },
         { path: 'cliente', data: { breadcrumb: 'Cliente' }, loadChildren: () => import('./Cliente/list-cliente/Clientedemo.module').then(m => m.ClienteDemoModule) },
