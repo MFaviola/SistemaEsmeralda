9827cbf467fd4ebfb7f28262776af4b2ff8d7eca
MFaviola <165686215+MFaviola@users.noreply.github.com>
2024-05-03 15:41:41 -0600

    www

--- sakai-ng-master/src/app/demo/components/generales/Cargo/list-cargo/list-cargo.component.ts
+++ sakai-ng-master/src/app/demo/components/generales/Cargo/list-cargo/list-cargo.component.ts
@@ -3,9 +3,10 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { Product } from 'src/app/demo/api/product';
 import {Router} from '@angular/router';
 import { Table } from 'primeng/table';
-import { Cargo } from 'src/app/Models/CargoViewModel';
 import { ServiceService } from 'src/app/Service/Cargo.service';
 import { FormGroup, FormControl,  Validators  } from '@angular/forms';
+import { MensajeViewModel } from 'src/app/Models/MensajeViewModel';
+import { Fill,Cargo, CargoEnviar } from 'src/app/Models/CargoViewModel';
 
 
 
@@ -17,65 +18,209 @@ import { FormGroup, FormControl,  Validators  } from '@angular/forms';
 export class ListCargoComponent implements OnInit{
   Cargo!:Cargo[];
    
-
-  statuses: any[] = [];
-
-  products: Product[] = [];
-  productDialog: boolean = false;
-  rowGroupMetadata: any;
+  MensajeViewModel!: MensajeViewModel[];
   submitted: boolean = false;
-
-  display: boolean = false;
-  activityValues: number[] = [0, 100];
-
-  isExpanded: boolean = false;
-
-  idFrozen: boolean = false;
-
   loading: boolean = false;
- 
+  departamentos: any[] = [];
+  fill: any[] = [];
+  viewModel: CargoEnviar = new CargoEnviar();
+  cargoForm: FormGroup;
   @ViewChild('filter') filter!: ElementRef;
+  Collapse: boolean = false;
+  DataTable: boolean = true;
+  Detalles: boolean = false;
+  Agregar: boolean = true;
+  MunCodigo: boolean = true;
+  Valor: string = "";
+  staticData = [{}];
 
-  selectedState: any = null;
 
-  states: any[] = [
-      {name: 'Arizona', code: 'Arizona'},
-      {name: 'California', value: 'California'},
-      {name: 'Florida', code: 'Florida'},
-      {name: 'Ohio', code: 'Ohio'},
-      {name: 'Washington', code: 'Washington'}
-  ];
-
-  dropdownItems = [
-      { name: 'Option 1', code: 'Option 1' },
-      { name: 'Option 2', code: 'Option 2' },
-      { name: 'Option 3', code: 'Option 3' },
-      { name: 'Option 3', code: 'Option 3' },
-      { name: 'Option 3', code: 'Option 3' },
-      
-  ];
-
-  cities1: any[] = [];
-
-  cities2: any[] = [];
-
-  city1: any = null;
-
-  city2: any = null;
-
-  constructor(private service: ServiceService, private router: Router
+  deleteProductDialog: boolean = false;
+  //Detalle
+  Carg: String = "";
   
-  ) { }
+  UsuarioCreacion: String = "";
+  UsuarioModificacion: String = "";
+  FechaCreacion: String = "";
+  FechaModificacion: String = "";
+  ID: String = "";
+
+  constructor(
+      private service: ServiceService, 
+      private router: Router,
+      private confirmationService: ConfirmationService, 
+      private messageService: MessageService
+  ) { 
+     
+  
+  }
+  
+
+
+
+
 
 
   ngOnInit(): void {
-      this.service.getCargo().subscribe((data: any)=>{
+      //Inicializamos form,drops,lista
+      this.cargoForm = new FormGroup({
+          Carg_Cargo: new FormControl("", Validators.required),
+        });
+    
+
+      this.service.getCargo().subscribe((data: Cargo[]) => {
           console.log(data);
           this.Cargo = data;
-      },error=>{
-        console.log(error);
       });
-   }
+  }
+  //Abrir collapse
+  collapse(){
+      this.Collapse= true;
+      this.DataTable = false;
+      this.Valor = "Agregar";
+      this.Agregar= false;
+      this.Detalles = false;
+  }
+  detalles(codigo){
+      this.Collapse= false;
+      this.DataTable = false;
+      this.Agregar= false;
+      this.Detalles = true;
+      this.service.getFill(codigo).subscribe({
+          next: (data: Fill) => {
+             this.Carg = data.carg_Cargo,
+             this.UsuarioCreacion = data.usuarioCreacion,
+             this.UsuarioModificacion = data.usuarioModificacion
+             this.FechaCreacion = data.fechaCreacion,
+             this.FechaModificacion = data.fechaModificacion
+          }
+        });
+  }
+  //Cerrar Collapse y reiniciar el form
+  cancelar(){
+      this.Collapse= false;
+      this.DataTable = true;
+      this.Detalles = false;
+      this.cargoForm = new FormGroup({
+          Carg_Cargo: new FormControl("", Validators.required),
+      });
+      this.submitted = false;
+      this.Agregar= true;
+      this.MunCodigo=true;
+      this.Valor = "";
+  }
+  //Funcionan como regex
+  ValidarNumeros(event: KeyboardEvent) {
+      if (!/[0-9]/.test(event.key) && event.key !== 'Backspace' && event.key !== 'Tab') {
+          event.preventDefault();
+      }
+  }
+  validarTexto(event: KeyboardEvent) {
+
+      if (!/^[a-zA-Z\s]+$/.test(event.key) && event.key !== 'Backspace' && event.key !== 'Tab' && event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
+          event.preventDefault();
+      }
+  }
+
+  //Insert
+  onSubmit() {
+  if (this.cargoForm.valid ) {
+     this.viewModel = this.cargoForm.value;
+     if (this.Valor == "Agregar") {
+      this.service.EnviarCargo(this.viewModel).subscribe((data: MensajeViewModel[]) => {
+          if(data["message"] == "Operación completada exitosamente."){
+           this.service.getCargo().subscribe((data: Cargo[]) => {
+               this.Cargo = data;
+           });
+           this.messageService.add({ severity: 'success', summary: 'Exito', detail: 'Insertado con Exito', life: 3000 });
+           this.Collapse= false;
+           this.DataTable = true;
+           this.submitted = false;
+           this.Detalles = false;
+           this.cargoForm = new FormGroup({
+               Carg_Cargo: new FormControl("", Validators.required),
+           });
+   
+          }else{
+           this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No se logro insertar', life: 3000 });
+          }
+          
+       })
+     } else {
+          this.service.ActualizarCargo(this.viewModel).subscribe((data: MensajeViewModel[]) => {
+          if(data["message"] == "Operación completada exitosamente."){
+           this.service.getCargo().subscribe((data: Cargo[]) => {
+               this.Cargo = data;
+           });
+           this.messageService.add({ severity: 'success', summary: 'Exito', detail: 'Actualizado con Exito', life: 3000 });
+           this.Collapse= false;
+           this.DataTable = true;
+           this.Detalles = false;
+           this.submitted = false;
+           this.cargoForm = new FormGroup({
+               Carg_Cargo: new FormControl("", Validators.required),
+           });
+   
+          }else{
+           this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No se logro actualizar', life: 3000 });
+          }
+          
+       })
+     }  
+     
+  }   
+      else 
+      {
+          this.submitted = true;
+      }
+  }
+
+
+  deleteSelectedProducts(codigo) {
+      this.deleteProductDialog = true;
+      this.ID = codigo;
+      console.log("El codigo es" + codigo);
+  }
+  confirmDelete() {
+      this.service.EliminarCargo(this.ID).subscribe({
+          next: (response) => {
+              if(response.message == "La accion ha sido existosa"){
+                  this.service.getCargo().subscribe((data: Cargo[]) => {
+                      this.Cargo = data;
+                  });
+                  this.messageService.add({ severity: 'success', summary: 'Exito', detail: 'Eliminado con Exito', life: 3000 });
+                  this.Collapse= false;
+                  this.DataTable = true;
+                  this.Detalles = false;
+                  this.submitted = false;
+                  this.cargoForm = new FormGroup({
+                      Carg_Cargo: new FormControl("", Validators.required),
+                  });
+                  this.deleteProductDialog = false;
+                 }else{
+                  this.deleteProductDialog = false;
+                  this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No se logro eliminar', life: 3000 });
+                 }
+          },
+      });
   
-  
+  }
+  Fill(codigo) {
+      this.service.getFill(codigo).subscribe({
+          next: (data: Fill) => {
+              this.cargoForm = new FormGroup({
+                  Carg_Cargo: new FormControl(data.carg_Cargo, Validators.required),
+              });
+              this.Collapse= true;
+              this.DataTable = false;
+              this.Agregar = false;
+              this.MunCodigo = false;
+              this.Detalles = false;
+              this.Valor = "Editar";
+          }
+        });
+
+  }
 }
+
+
