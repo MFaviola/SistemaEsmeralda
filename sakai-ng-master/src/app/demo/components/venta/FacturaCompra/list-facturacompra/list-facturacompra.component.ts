0df746541249a06750a47912a06eeeeab3dc8113
Jason Jeremy Villanueva Sanchez <161340354+jahirmjlg@users.noreply.github.com>
2024-05-08 15:35:01 -0600

    factura compra carpeta

---
+++ sakai-ng-master/src/app/demo/components/venta/FacturaCompra/list-facturacompra/list-facturacompra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-facturacompra',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-facturacompra.component.html',
+  styleUrl: './list-facturacompra.component.scss'
+})
+export class ListFacturacompraComponent {
+
+}
