2e51b25f484c43fd034848857d88adfc6cceceaa
Jason Jeremy Villanueva Sanchez <161340354+jahirmjlg@users.noreply.github.com>
2024-05-09 16:15:30 -0600

    .

--- sakai-ng-master/src/app/Service/factura-compra.service.ts
+++ sakai-ng-master/src/app/Service/factura-compra.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BASE_URL } from './ulrsettings';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs';
+import { FacturaCompraEncabezado } from '../Models/FacturaCompraViewModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturaCompraService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  url = BASE_URL + 'API/FacturaCompra/'
+
+  getFacturaCompra(){
+    return this.http.get<FacturaCompraEncabezado[]>(this.url)
+  }
+
 }
